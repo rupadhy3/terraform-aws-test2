856c86dd5dec7b69b6c117ff84b7cf2daf411d85
root <rakesh.upadhyay1@ibm.com>
2021-10-29 10:20:23 +0000

    commit 3

--- provider.tf
+++ provider.tf
@@ -31,13 +31,13 @@ variable "pubkey" {
 }
 
 resource "aws_instance" "myservers" {
-   ami = ${ var.environment == "prod" ? var.ami_id_redhat : var.ami_id_ubuntu }
-   instance_type = ${ var.environment == "prod" ? var.it_redhat : var.it_ubuntu }
+   ami = "${ var.environment == "prod" ? var.ami_id_redhat : var.ami_id_ubuntu }"
+   instance_type = "${ var.environment == "prod" ? var.it_redhat : var.it_ubuntu }"
    key_name = aws_key_pair.mykeypair.key_name
 
 
    tags = {
-      Name = ${var.server}-28oct
+      Name = "${var.server}-28oct"
       env = "test"
    }
 }
